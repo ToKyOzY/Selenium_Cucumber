a2e8ffc900af084ed0bc9d9b14cbf3e12162bb08
Zeynep Sarıkaya <zynpsarkaya@gmail.com>
2022-10-07 22:23:27 +0300

    cucumber

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,36 @@
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "stepDefinitions",
+        tags = "@gp1 or @gp2",
+        dryRun = false
+
+)
+
+public class Runner {
+    /*
+    Runner class'i testng'deki xml mantigi ile calisir.
+    Calistirmak istedigimiz senaryolara tag belirtiriz ve belirttigimiz tag'lari calistirir.
+    XML'deki gibi istedigmiz testleri calistirmak icin kullaniriz.
+    Runner class body'si bostur ve runner class'ini ekleyecegimiz notasyonlar active eder.
+    Bu class'da kullanacagimiz 2 adet notasyon vardir.
+    1.@RunWith(Cucumber.class) notasyonu Runner class'ina calisma özelligi ekler.
+    bu notasyon oldugu icin Cucumber Framework'umuzde Junit kullanmayi tercih ederiz
+    2. @CucumberOptions notasyonu içinde features : runner dosyasinin feature dosyasina nereden alacagini
+    tarif eder.
+    glue : stepDefinitions yolunu belirtiriz
+    tags : hangi tag'i calistirmak istiyorsak onu belli eder
+
+    dryRun : iki secenek vardir
+    dryRun=true dersek testimizi calistirmadan eksik adimlari bize verir
+    dryRun=false dersek testlerimizi river ile calistirir
+
+     */
+}
--- src/test/java/stepDefinitions/AmazonStepDefinition.java
+++ src/test/java/stepDefinitions/AmazonStepDefinition.java
@@ -3,27 +3,53 @@ package stepDefinitions;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import org.openqa.selenium.By;
+import org.junit.Assert;
 import org.openqa.selenium.Keys;
+import pages.AmazonPage;
+import utilities.ConfigReader;
 import utilities.Driver;
 
 public class AmazonStepDefinition {
+    AmazonPage amazonPage=new AmazonPage();
     @Given("Kullanici amazon sayfasina gider")
     public void kullaniciAmazonSayfasinaGider() {
-        Driver.getDriver().get("https://www.amazon.com");
+        Driver.getDriver().get(ConfigReader.getProperty("amazon_Url"));
     }
 
     @Then("Kullanici Nutella icin arama yapar")
     public void kullaniciNutellaIcinAramaYapar() {
-        Driver.getDriver().findElement(By.id("twotabsearchtextbox")).sendKeys("nutella", Keys.ENTER);
+      amazonPage.searchBox.sendKeys("nutella",Keys.ENTER);
     }
 
     @And("Sonuclarin nutella icerdigini test eder")
     public void sonuclarinNutellaIcerdiginiTestEder() {
+      Assert.assertTrue(amazonPage.resultSearch.getText().contains("nutella"));
 
     }
 
     @And("Sayfayi kapatir")
     public void sayfayiKapatir() {
+        Driver.closeDriver();
+    }
+
+    @Then("kullanici selenium icin arama yapar")
+    public void kullaniciSeleniumIcinAramaYapar() {
+
+        amazonPage.searchBox.sendKeys("selenium",Keys.ENTER);
+    }
+
+    @And("Sonuclarin selenium icerdigini test eder")
+    public void sonuclarinSeleniumIcerdiginiTestEder() {
+        Assert.assertTrue(amazonPage.resultSearch.getText().contains("selenium"));
+    }
+
+    @Then("Kullanici iphone icin arama yapar")
+    public void kullaniciIphoneIcinAramaYapar() {
+        amazonPage.searchBox.sendKeys("iphone",Keys.ENTER);
+    }
+
+    @And("Sonuclarin iphone icerdigini test eder")
+    public void sonuclarinIphoneIcerdiginiTestEder() {
+        Assert.assertTrue(amazonPage.resultSearch.getText().contains("iphone"));
     }
 }
